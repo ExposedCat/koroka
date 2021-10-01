d656997ef7674453cd682945ad318cf255eb324d
ExposedCat <artem13.prokop@gmail.com>
2021-10-01 19:29:50 +0300

    Fix code style (nesting) errors

--- src/services/users.js
+++ src/services/users.js
@@ -3,23 +3,24 @@ import { apiEndpoint } from '../config/manifest.js'
 
 
 async function getUsers(page = 1) {
+    let response
     try {
-        const response = await axios.get(`${apiEndpoint}?page=${page}`)
-        if (response.status === 200) {
-            const { total_pages: totalPages, page: currentPage, data } = response.data
-            return {
-                users: data,
-                nextPage: totalPages > currentPage ? currentPage + 1 : null
-            }
-        } else {
-            console.error(response)
-            return {
-                users: [],
-                nextPage: nulltotalPages > currentPage ? currentPage + 1 : null
-            }
-        }
+        response = await axios.get(`${apiEndpoint}?page=${page}`)
     } catch (error) {
         console.error(error)
+        return {
+            users: [],
+            nextPage: nulltotalPages > currentPage ? currentPage + 1 : null
+        }
+    }
+    if (response.status === 200) {
+        const { total_pages: totalPages, page: currentPage, data } = response.data
+        return {
+            users: data,
+            nextPage: totalPages > currentPage ? currentPage + 1 : null
+        }
+    } else {
+        console.error(response)
         return {
             users: [],
             nextPage: nulltotalPages > currentPage ? currentPage + 1 : null
